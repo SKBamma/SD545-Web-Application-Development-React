6e39b2a8ef401888ea0679419832cfa6df37b26c
Suresh K Bamma <116139359+SKBamma@users.noreply.github.com>
2024-04-13 23:02:49 -0500

    Workshop 3 todos completed.

---
+++ Workshop-3 Todos/src/components/Footer/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { Todo } from '../../types';
+
+import './index.css';
+
+
+type Props = {
+    todos: Todo[];
+};
+
+export default function Footer(props: Props) {
+    const { todos } = props;
+    const changeCheckBox = () => {
+
+    };
+    return (
+        <div className="todo-footer">
+            <label>
+                <input type="checkbox" checked={todos.filter(todo => todo.done).length === todos.length
+                    && todos.length !== 0} onChange={changeCheckBox} />
+            </label>
+            <span>
+                <span> Finished {todos.filter(todo => todo.done).length} </span> / total {todos.length}
+            </span>
+            <button className="btn btn-danger">Delete Finished Tasks</button>
+        </div>
+    );
+}
